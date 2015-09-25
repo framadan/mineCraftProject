902dbe9c1042044ea6ee6f9a768a210fe73c573c
framadan <mlginsurance@yahoo.com>
2015-09-25 15:05:15 -0700

    Inventory

--- Farris Ramadan/New Unity Project/Assets/InventorySpace.cs
+++ Farris Ramadan/New Unity Project/Assets/InventorySpace.cs
@@ -3,16 +3,23 @@ using System.Collections;
 
 public class InventorySpace : MonoBehaviour 
 {
+	public GameObject[] spaces;
+	public int sCount = 0;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
-	{
-	
+	{ 
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Debug.Log (spaces[sCount]);
+			sCount++;
+
+		}
 	}
 }
---
+++ Farris Ramadan/New Unity Project/Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public GameObject inventoryPanel = null;
+	public bool inventoryActive = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+			if (Input.GetKeyDown (KeyCode.E)) 
+		{
+			inventoryPanel.SetActive(inventoryActive);
+			inventoryActive = !inventoryActive;
+		}
+
+		
+	}
+}
