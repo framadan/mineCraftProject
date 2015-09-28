d0aed354d3a62f4005c86cd3be7a2d71c043620a
Chadwikowezy <chad_mcleavy@hotmail.com>
2015-09-28 09:19:06 -0700

    The play can destroy blocks
    The player can now destroy blocks after a certain amount of time based
    on the blocks destroy timer.

---
+++ Chad McLeavy/New Unity Project/Assets/Scripts/Block.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour 
+{
+	public float breakTimerReset = 3.0f;
+	public float breakTimer = 3.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		BlockBreak ();
+	}
+
+	void BlockBreak ()
+	{
+		if (breakTimer <= 0.0f) 
+		{
+			Destroy (gameObject,0.0f);
+		}
+	}
+}
--- Chad McLeavy/New Unity Project/Assets/Scripts/Player.cs
+++ Chad McLeavy/New Unity Project/Assets/Scripts/Player.cs
@@ -3,71 +3,136 @@ using System.Collections;
 
 public class Player : MonoBehaviour 
 {
+	public float movementSpeed = 0.0f;
+	public float mouseSensitivity = 0.0f;
+	public float jumpVelocity = 0.0f;
+
+	float verticalRotation = 0.0f;
+	public float upDownRotationLimit = 0.0f;
+
+	float verticalVelocity = 0.0f;
+
+	//PlayerIdol
+	//public float timeSpentIdol1 = 0.0f;
+	//public float timeSpentIdol2 = 0.0f;
+
 	public int health = 20;
 	public int attack = 1;
 
-	public float speed = 1.0f;
-	public float jumpHeight = 1.0f;
 	public float distance = 8.1f;
 
-	public bool canPlaceBlocks = false;
-
-	public GameObject testCube = null;
-
+	public GameObject block = null;
+	
 	// Use this for initialization
 	void Start () 
 	{
-		
+
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
-		Movement ();
 		PlaceBlocks ();
-	}
+		BreakBlocks ();
 
-	void Movement ()
-	{
-		if (Input.GetKey (KeyCode.W))
+		CharacterController cc = GetComponent<CharacterController>();
+
+		//Rotation
+
+		float leftRightRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
+		transform.Rotate (0, leftRightRotation, 0);
+
+		verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+		verticalRotation = Mathf.Clamp(verticalRotation, -upDownRotationLimit, upDownRotationLimit);
+		Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+
+		//Movement
+	
+		float fowardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+
+		verticalVelocity += Physics.gravity.y * Time.deltaTime;
+
+		if(cc.isGrounded && Input.GetButtonDown("Jump"))
 		{
-			transform.Translate (Vector3.forward * Time.deltaTime * speed);
+			verticalVelocity = jumpVelocity;
 		}
-		if (Input.GetKey (KeyCode.S))
-		{
-			transform.Translate (Vector3.back * Time.deltaTime * speed);
-		}
-		if (Input.GetKey (KeyCode.A))
-		{
-			transform.Translate (Vector3.left * Time.deltaTime * speed);
-		}
-		if (Input.GetKey (KeyCode.D))
-		{
-			transform.Translate (Vector3.right * Time.deltaTime * speed);
-		}
-		if (Input.GetKeyDown (KeyCode.Space))
-		{
-			GetComponent<Rigidbody>().AddForce (Vector3.up * jumpHeight);
-		}
+
+		Vector3 speed = new Vector3(sideSpeed, verticalVelocity, fowardSpeed);
+
+		speed = transform.rotation * speed;
+
+		cc.Move(speed * Time.deltaTime);
+
+		//Player Idol
+		//Note: Code Works Just Messy
+
+		//if (Mathf.Abs(Input.GetAxis("Vertical")) <= 0.1)
+		//{
+		//
+		//	if(timeSpentIdol1 >= 0)
+		//	{
+		//		timeSpentIdol1 = timeSpentIdol1 - 1 * Time.deltaTime;
+		//		if(timeSpentIdol1 <= 0)
+		//		{
+		//			GetComponent<AudioSource>().Play();
+		//		}
+		//	}
+		//
+		//	if(timeSpentIdol1 <=0)
+		//	{
+		//		timeSpentIdol2 = timeSpentIdol2 - 1 * Time.deltaTime;
+		//		if(timeSpentIdol2 <= 0)
+		//		{
+		//			GetComponent<AudioSource>().Play ();
+		//		}
+		//	}
+		//}
+		
 	}
 
 	void PlaceBlocks ()
 	{
-		if (Physics.Raycast (transform.position, Vector3.forward, distance)) 
+		if (Input.GetKeyDown (KeyCode.Mouse1)) 
 		{
-			canPlaceBlocks = true;
-		}
-		if (Physics.Raycast (transform.position, Vector3.forward, distance) != true) 
-		{
-			canPlaceBlocks = false;
-		}
-		if (Input.GetKeyDown (KeyCode.Mouse1) && canPlaceBlocks == true) 
-		{
-			Instantiate(testCube,transform.position,transform.rotation);
+			Ray rayOrigin = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hitInfo;
+
+			if (Physics.Raycast (rayOrigin, out hitInfo, 8.0f))
+			{
+				Instantiate (block, hitInfo.point + Vector3.up * .5f, hitInfo.collider.transform.rotation);
+			}
 		}
 	}
 
-	//For when hotbar is ready
+	void BreakBlocks ()
+	{
+		if (Input.GetKey (KeyCode.Mouse0)) 
+		{
+			Ray rayOrigin = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hitInfo;
+			
+			if (Physics.Raycast (rayOrigin, out hitInfo, 8.0f))
+			{
+				Debug.Log ("Success");
+				hitInfo.collider.gameObject.GetComponent<Block>().breakTimer -= Time.deltaTime;
+			}
+		}
+		if (Input.GetKeyUp (KeyCode.Mouse0)) 
+		{
+			Ray rayOrigin = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hitInfo;
+			
+			if (Physics.Raycast (rayOrigin, out hitInfo, 8.0f))
+			{
+				Debug.Log ("Success");
+				hitInfo.collider.gameObject.GetComponent<Block>().breakTimer = 
+				hitInfo.collider.gameObject.GetComponent<Block>().breakTimerReset;
+			}
+		}
+	}
+
 	void HotbarSwitch ()
 	{
 		if (Input.GetKeyDown (KeyCode.Keypad1)) 
