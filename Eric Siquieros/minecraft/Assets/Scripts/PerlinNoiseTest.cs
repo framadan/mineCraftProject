bcc239166d802332c212455c39cd41016d1a4b11
SERVER1992 <esiqueiros@student.rcc.edu>
2015-09-30 22:18:51 -0700

    Perlin Noise Test Script
    Added another script to test out a different method of adding Perlin
    Noise.

---
+++ Eric Siquieros/minecraft/Assets/Scripts/PerlinNoiseTest.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PerlinNoiseTest : MonoBehaviour
+{
+	public GameObject dirtBlock = null;
+
+	public int size = 50;
+	public float scale = 7.0f;
+	public float scaleModifier = 5.0f;
+	public float offSetHeight = 1.5f;
+
+	public bool enableHeight = false;
+	public bool move = false;
+
+	void Start()
+	{
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,0,z), Quaternion.identity) as GameObject;
+
+				c.transform.parent = transform;
+			}
+		}
+	}
+
+	void Update ()
+	{
+		updateTransform();
+	}
+
+	void updateTransform()
+	{
+		foreach(Transform child in transform)
+		{
+			var height = Mathf.PerlinNoise(child.transform.position.x/scale, child.transform.position.z/scale);
+
+			setMatColor(child, height);
+
+			if(enableHeight == true)
+				applyHeight(child, height);
+		}
+	}
+
+	void setMatColor(Transform child, float height)
+	{
+		child.GetComponent<Renderer>().material.color = new Color(height,height,height,height);
+	}
+
+	void applyHeight(Transform child, float height)
+	{
+		var yValue = Mathf.RoundToInt(height * scaleModifier);
+
+		var newVec3 = new Vector3(child.transform.position.x, yValue, child.transform.position.z);
+
+		child.transform.position = newVec3;
+	}
+}
