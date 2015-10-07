c09250ed35e70289bfc58e6823c9b5ca3ad6d0b8
Chadwikowezy <chad_mcleavy@hotmail.com>
2015-10-07 14:07:11 -0700

    There is now a fully functional hotbar
    The player can now pick up items and place them in the hotbar. The hot
    bar is scrolled through with the scroll wheele. The player can also
    place different blocks based on what is selected in the hotbar. There is
    also a sound when the player places a block now.

---
+++ Chad McLeavy/Minecraft/Assets/Scripts/Block.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour 
+{
+	public float breakTimerReset = 3.0f;
+	public float breakTimer = 3.0f;
+
+	public GameObject blockDrop;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		BlockBreak ();
+	}
+
+	void BlockBreak ()
+	{
+		if (breakTimer <= 0.0f) 
+		{
+			Instantiate (blockDrop,transform.position,transform.rotation);
+			Destroy (gameObject,0.0f);
+		}
+	}
+}
---
+++ Chad McLeavy/Minecraft/Assets/Scripts/HealthGUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthGUI : MonoBehaviour 
+{
+	public Text Health;
+
+	public string health;
+	
+	public GameObject player = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Health.text = health + player.GetComponent<DeathCode>().health;
+	}
+}
