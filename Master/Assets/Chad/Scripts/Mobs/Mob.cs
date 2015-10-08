34a0e67173d070d04a502db12bcc1b44e3f85001
framadan <mlginsurance@yahoo.com>
2015-10-07 19:00:13 -0700

    Update

---
+++ Master/Assets/Chad/Scripts/Mobs/Mob.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mob : MonoBehaviour 
+{
+	public float speed = 1.0f;
+	public float directionTimer = 0.0f;
+	public float directionChange = 0.0f;
+	public float jumpHeight = 75f;
+	public float detectDistance = 0.5f;
+
+	public Quaternion forwardDirection = Quaternion.Euler (0f,0f,0f);
+	public Quaternion backDirection = Quaternion.Euler (0f,180f,0f);
+	public Quaternion leftDirection = Quaternion.Euler (0f,90f,0f);
+	public Quaternion rightDirection = Quaternion.Euler (0f,-90f,0f);
+
+	public bool canJump = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+		directionChange = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		directionTimer += Time.deltaTime;
+
+		ChangeDirection ();
+		DetectBlock ();
+	}
+
+	void ChangeDirection () 
+	{
+
+		if (directionTimer > 5)
+		{
+			directionTimer = 0;
+			directionChange = Random.Range (0f, 8f);
+		}
+
+		//Changing Directions
+		if (directionChange >= 0 && directionChange < 2)
+		{
+			transform.rotation = Quaternion.Slerp (transform.rotation,forwardDirection, 5f * Time.deltaTime);
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		}
+		if (directionChange >= 2 && directionChange < 4)
+		{
+			transform.rotation = Quaternion.Slerp (transform.rotation,backDirection, 5f * Time.deltaTime);
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		}
+		if (directionChange >= 4 && directionChange < 6)
+		{
+			transform.rotation = Quaternion.Slerp (transform.rotation,leftDirection, 5f * Time.deltaTime);
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		}
+		if (directionChange >= 6 && directionChange < 8)
+		{
+			transform.rotation = Quaternion.Slerp (transform.rotation,rightDirection, 5f * Time.deltaTime);
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		}
+	}
+
+	void DetectBlock ()
+	{
+		Ray rayOrigin = new Ray (transform.position,transform.forward);
+		RaycastHit hitInfo;
+		if (Physics.Raycast (rayOrigin, out hitInfo, detectDistance)) 
+		{
+			if (hitInfo.collider.gameObject.tag == "Block" && canJump == true)
+			{
+				canJump = false;
+				GetComponent<Rigidbody>().AddForce (transform.up * jumpHeight);
+			}
+		}
+		if (!Physics.Raycast (rayOrigin, out hitInfo, detectDistance)) 
+		{
+			canJump = true;
+		}
+	}
+}
---
+++ Master/Assets/EricSiqueiros/Scripts/PerlinNoiseTest1.cs
@@ -0,0 +1,372 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class PerlinNoiseTest1 : MonoBehaviour
+{
+	public GameObject dirtBlock = null;
+	public GameObject stoneBlock = null;
+
+	public int size = 50;
+	public float scale = 7.0f;
+	public float scaleModifier = 5.0f;
+	public float offSetHeight = 1.5f;
+
+	private int randomLayer = 0;
+
+	public List<GameObject> squares = new List<GameObject> ();
+
+	public bool enableHeight = true;
+	public bool move = false;
+
+	void Start()
+	{
+		randomLayer = Random.Range(1,3);
+
+		if(randomLayer ==1 )
+		{
+			LayerOne();
+		}
+
+		if(randomLayer == 2)
+		{
+			LayerTwo();
+		}
+
+		if(randomLayer == 3)
+		{
+			LayerTwo();
+		}
+
+	}
+
+	void LayerOne()
+	{
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,2,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,1,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,0,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,-1,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,-2,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,-3,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(stoneBlock, new Vector3(x,-4,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(stoneBlock, new Vector3(x,-5,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+	}
+
+	void LayerTwo()
+	{
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,2,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,1,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(stoneBlock, new Vector3(x,0,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,-1,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(stoneBlock, new Vector3(x,-2,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,-3,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(stoneBlock, new Vector3(x,-4,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(stoneBlock, new Vector3(x,-5,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+	}
+
+	void LayerThree()
+	{
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,2,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,1,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,0,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,-1,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(stoneBlock, new Vector3(x,-2,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,-3,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(stoneBlock, new Vector3(x,-4,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+		
+		for(var x = 0; x < size; x++)
+		{
+			for(var z = 0; z < size; z++)
+			{
+				var c = Instantiate(dirtBlock, new Vector3(x,-5,z), Quaternion.identity) as GameObject;
+				squares.Add(c);
+				
+				c.transform.parent = transform;
+				
+			}
+		}
+	}
+
+	void Update ()
+	{
+		updateTransform();
+	}
+
+	void updateTransform()
+	{
+		foreach(Transform child in transform)
+		{
+			var height = Mathf.PerlinNoise(child.transform.position.x/scale, child.transform.position.z/scale);
+
+			setMatColor(child, height);
+
+			if(enableHeight == true)
+				applyHeight(child, height);
+		}
+	}
+
+	void setMatColor(Transform child, float height)
+	{
+		//child.GetComponent<Renderer>().material.color = new Color(height,height,height,height);
+	}
+
+	void applyHeight(Transform child, float height)
+	{
+		var yValue = Mathf.RoundToInt(height * scaleModifier);
+
+		var newVec3 = new Vector3(child.transform.position.x, yValue, child.transform.position.z);
+
+		child.transform.position = newVec3;
+	}
+}
