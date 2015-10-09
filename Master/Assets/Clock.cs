d4f3780401c3510aa2fd68d2fd34f3b52926a907
SERVER1992 <esiqueiros@student.rcc.edu>
2015-10-08 22:53:38 -0700

    Adding Real Day Night Cycle
    Just added Erik's Script to the Master

---
+++ Master/Assets/Clock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Clock : MonoBehaviour {
+	
+	public Transform hourHand;
+	public Transform minuteHand;
+	
+	float hoursToDegrees = 360f / 12f;
+	float minutesToDegrees = 360f / 60f;
+	DayNightController controller;
+	
+	void Awake() {
+		controller = GameObject.Find("DayNightController").GetComponent<DayNightController>();
+	}
+	
+	void Update() {
+		float currentHour = 24 * controller.currentTimeOfDay;
+		float currentMinute = 60 * (currentHour - Mathf.Floor(currentHour));
+		
+		hourHand.localRotation = Quaternion.Euler(currentHour * hoursToDegrees, 0, 0);
+		minuteHand.localRotation = Quaternion.Euler(currentMinute * minutesToDegrees, 0, 0);
+	}
+}
